6397a6435364c771d3d57cfeb4660799dec73e1a
b0g <boris.gallet@gmail.com>
2014-06-04 03:57:25 +0200

    J'ai fait beaucoup de fonction mais ça ne fonction pas chez moi, impossible de tester, mais au moins c'est ça de fait :) 	modified:   pom.xml 	modified:   src/main/java/com/sil6/client/Client1.java 	modified:   src/main/java/com/sil6/controller/SecondService.java 	modified:   src/main/java/com/sil6/controller/SecondServiceImpl.java 	modified:   src/main/java/com/sil6/model/ThirdService.java 	modified:   src/main/java/com/sil6/model/ThirdServiceImpl.java 	modified:   src/main/java/com/sil6/restjson/RestJsonApplication.java 	modified:   src/main/java/com/sil6/v1/ressources/Croak.java 	modified:   src/main/java/com/sil6/v1/ressources/Croakos.java

--- src/main/java/com/sil6/controller/SecondService.java
+++ src/main/java/com/sil6/controller/SecondService.java
@@ -6,14 +6,18 @@
 
 package com.sil6.controller;
 
+import com.sil6.v1.ressources.Croak;
 import com.sil6.v1.ressources.Croakos;
 import java.rmi.Remote;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBElement;
 
 public interface SecondService extends Remote{
     
@@ -39,17 +43,42 @@ public interface SecondService extends Remote{
     
     //Fonction de connexion (verification que le client existe)
     //Les login et mdp sont passé en parametre
+    @GET
+    @Path("connexion/{name}/{mdp}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Croakos connexion(@PathParam("name") String name, @PathParam("mdp") String mdp);
     
     //Inscription (verification que le client n'existe pas deja)
-    //Login et mdp en paremetre
+    //Login et mdp en parametre
+    @PUT
+    @Path("inscription/{name}/{mdp}")
+    @Consumes("plain/text")
+    public boolean inscription(@PathParam("name") String name, @PathParam("mdp") String mdp);
     
-    //Poste d'un tweet
-    //Message en parametre
+    /**
+     * Poste d'un tweet
+     * Message en parametre
+     * @Retour boolean
+     */
+    @PUT
+    @Path("postCroak/{croak}")
+    @Consumes(MediaType.APPLICATION_XML)
+    public boolean postCroak (@PathParam("croak") JAXBElement<Croak> croak);
     
-    //Abonnement a un utilisateur (mettre à jour les abonnements etc.. + verifie l'existence)
-    //Nom de l'utilisateur où s'abonner en param
+    
+    /**
+     *Abonnement a un utilisateur (mettre à jour les abonnements etc.. + verifie l'existence)
+     *Nom de l'utilisateur où s'abonner en param + nom de celui qui s'abonne
+    */
+    @PUT
+    @Path("abonnement/{nameUser}/{nameAbo}")
+    @Consumes("text/plain")
+    public boolean abonnement(@PathParam("nameUser") String nameUser, @PathParam("nameAbo") String nameAbo);
     
     //Recuperation de la liste des tweets (les tweets de ses abonnement)
     //Nom d'utilisateur courrant en parametre
-    
+    @GET
+    @Path("getCroaks/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Croak> getCroaks(@PathParam("name") String name);
 }
--- src/main/java/com/sil6/controller/SecondServiceImpl.java
+++ src/main/java/com/sil6/controller/SecondServiceImpl.java
@@ -7,6 +7,7 @@
 package com.sil6.controller;
 
 import com.sil6.model.ThirdService;
+import com.sil6.v1.ressources.Croak;
 import com.sil6.v1.ressources.Croakos;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -22,12 +23,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBElement;
 
 // Set the path, version 1 of API
 @Path("/")
 public class SecondServiceImpl extends Thread implements SecondService, Remote{
 	
         private ThirdService thirdService = null;
+        
 
         /* Constructor : connexion with RMI to the third tiers */
         public SecondServiceImpl() {
@@ -57,9 +60,9 @@ public class SecondServiceImpl extends Thread implements SecondService, Remote{
         
 
         @Override
-        @GET
-        @Path("getUser/{name}")
-        @Produces({MediaType.APPLICATION_JSON})
+        //@GET
+        //@Path("getUser/{name}")
+        //@Produces({MediaType.APPLICATION_JSON})
         public Croakos getUser(@PathParam("name") String name) {
             Croakos user = null;
             System.out.println(name);
@@ -71,6 +74,79 @@ public class SecondServiceImpl extends Thread implements SecondService, Remote{
                 }
             }
             return user;
+        }
+        
+        @Override
+        @GET
+        @Path("connexion/{name}/{mdp}")
+        @Produces({MediaType.APPLICATION_JSON})
+        public Croakos connexion(@PathParam("name") String name, @PathParam("mdp") String mdp) {
+            Croakos user = null;
+            System.out.println(name);
+            if(thirdService != null){
+                try {
+                    user = thirdService.getUser(name);
+                    if(user.getMdp()!= mdp ){
+                        user = null;
+                    }
+                } catch (RemoteException ex) {
+                    Logger.getLogger(SecondServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            return user;
+        }
+
+        @Override
+        public boolean inscription(String name, String mdp) {
+            boolean bool = false;
+            if(this.getUser(name) == null){
+                Croakos user = new Croakos(name, mdp);
+                if(thirdService != null){
+                    try {
+                        thirdService.saveUser(user);
+                        bool=true;
+                    } catch (RemoteException ex) {
+                        Logger.getLogger(SecondServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            else{
+                bool = false; //Si l'utilisateur existe déjà
+            }
+            return bool;
+        }
+
+        @Override
+        public boolean postCroak(JAXBElement<Croak> croak) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean abonnement(String nameUser, String nameAbo) {
+
+            Croakos follower = this.getUser(nameUser);
+            Croakos following = this.getUser(nameAbo);
+            
+            if(follower != null && following != null){
+                List<Croakos> followings = follower.getFollowing();
+                List<Croakos> followers = following.getFollowers();
+
+                followings.add(following);
+                followers.add(follower);
+
+                follower.setFollowing(followings);
+                following.setFollowers(followers);
+
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        @Override
+        public List<Croak> getCroaks(String name) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
 }
 
